8f9a42723aa0ce34acff0c60ec432cb46651f254
ShogunPanda <shogun_panda@me.com>
2012-10-22 08:45:53 +0200

    Version 1.2.5 - Fix required redcarpet version

--- lib/bovem/version.rb
+++ lib/bovem/version.rb
@@ -17,7 +17,7 @@ module Bovem
     MINOR = 2
 
     # The patch version.
-    PATCH = 4
+    PATCH = 5
 
     # The current version number of Bovem.
     STRING = [MAJOR, MINOR, PATCH].compact.join(".")
